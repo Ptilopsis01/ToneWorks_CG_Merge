7c66408785980464c1a8b06c3ae4915284e088c7
huangmai <hmskez@163.com>
2022-06-04 02:44:34 +0800

    tone work's merge v1.0beta

--- CGscripts/銀遥_月彼_SSR_CGmerge.py
+++ CGscripts/銀遥_月彼_SSR_CGmerge.py
@@ -2,7 +2,6 @@ from PIL import Image
 import os
 import shutil
 import fnmatch
-# 银遥有bug，勿用
 
 file_path = './cg/'
 if not os.path.exists('./output/'):
@@ -26,15 +25,6 @@ while n < len(txt_path):
             return merge_list
 
 
-    def ex2_judge(num):
-        if num < 3:
-            return 0
-        elif num == 3:
-            return 1
-        elif num == 4:
-            return 2
-
-
     cg_list = read_txt()
     length = len(cg_list) - 2
     status = []  # 标记cg是否已合成
@@ -45,17 +35,22 @@ while n < len(txt_path):
     while j < length:
         status.append(False)
         cg_ex.append(0)
-        if 'ex' in cg_list[j]:  # 如有ex图层，分为ex、ex1、ex1+ex2三种情况
-            if cg_list[j][-2:] == 'ex':
+        if 'ex' in cg_list[j]:  # 如有ex图层，分为ex/ex1、ex/ex1+ex2四种情况
+            if cg_list[j][-2:] == 'ex' and cg_list[j + 1][-3:] != 'ex2':
                 cg_ex[j] = 1
                 del cg_list[j]
                 length = length - 1
-            elif cg_list[j][-3:] == 'ex1' and cg_list[j + 1][-3:] != 'ex2':
+            elif cg_list[j][-2:] == 'ex' and cg_list[j + 1][-3:] == 'ex2':
                 cg_ex[j] = 2
+                del cg_list[j]
+                del cg_list[j]
+                length = length - 2
+            elif cg_list[j][-3:] == 'ex1' and cg_list[j + 1][-3:] != 'ex2':
+                cg_ex[j] = 1
                 del cg_list[j]
                 length = length - 1
             elif cg_list[j][-3:] == 'ex1' and cg_list[j + 1][-3:] == 'ex2':
-                cg_ex[j] = 3
+                cg_ex[j] = 2
                 del cg_list[j]
                 del cg_list[j]
                 length = length - 2
@@ -80,16 +75,16 @@ while n < len(txt_path):
         # 直接移动已有的图片
         if not status[j]:
             print(cg_list[j], 'is merging...')
-            print(cg_ex[j])
             name1, name2, full_index = cg_list[j].split('_', 2)
             index1 = int(full_index[0:2]) - 1
-            index2 = 0
-            index3 = 0
+            index2 = 100
+            index3 = 100
             if len(full_index) >= 4:
                 index2 = int(full_index[2:4]) - 1
             if len(full_index) >= 6:
                 index3 = int(full_index[4:6]) - 1
-            base = Image.new('RGBA', (1280, 720), (0, 0, 0, 0))
+            img_size = Image.open(file_path + all_file[0]).size
+            base = Image.new('RGBA', img_size, (0, 0, 0, 0))
             face1 = Image.new('RGBA', base.size, (0, 0, 0, 0))
             face2 = Image.new('RGBA', base.size, (0, 0, 0, 0))
             for i in all_file:  # 找到合成所需的CG图层
@@ -97,14 +92,14 @@ while n < len(txt_path):
                 part1 = all_parts[0]
                 part2 = all_parts[1]
                 part3 = ''
-                m = 0
                 if len(all_parts) >= 3:
                     m = 2
                     while m < len(all_parts):
                         part3 = part3 + all_parts[m]
                         m = m + 1
                 if part2 == name2:
-                    if part3 == 'base#%03d.png' % index1:
+                    if part3 == 'base#%03d.png' % index1 \
+                            or part3 == 'base%03d.png' % index1:
                         base = Image.open(file_path + i)
                         print(part3, 'BASE FOUND')
                     elif part3 == 'base.png':
@@ -117,17 +112,16 @@ while n < len(txt_path):
                         face2 = Image.open(file_path + i)
                         print(part3, 'FACE2 FOUND')
             base = base.convert('RGBA')
-            if cg_ex[j] == 0:  # 判断是否有ex图层
-                base.alpha_composite(face1, (0, 0))
-                base.alpha_composite(face2, (0, 0))
-                base.save(save_path + cg_list[j] + '.png')
-                print(cg_list[j] + '.png MERGE COMPLETE\n')
-            if cg_ex[j] == 1 or cg_ex[j] == 2 or cg_ex[j] == 3:
-                if full_index[4:6] == '':
-                    index3 = index2
+            base.alpha_composite(face1, (0, 0))
+            base.alpha_composite(face2, (0, 0))
+            if cg_ex[j] == 0:
+                if base != Image.new('RGBA', img_size, (0, 0, 0, 0)):
+                    base.save(save_path + cg_list[j] + '.png')
+                    print(cg_list[j] + '.png MERGE COMPLETE\n')
+                else:
+                    print('merge error\n')
+            elif cg_ex[j]:  # 如果有ex图层
                 for i in all_file:
-                    ex1 = Image.new('RGBA', base.size, (0, 0, 0, 0))
-                    ex2 = Image.new('RGBA', base.size, (0, 0, 0, 0))
                     all_parts = i.split('_')
                     part1 = all_parts[0]
                     part2 = all_parts[1]
@@ -137,28 +131,22 @@ while n < len(txt_path):
                         while m < len(all_parts):
                             part3 = part3 + all_parts[m]
                             m = m + 1
-                    if part2 == name2 and 'ex' in part3 and 'ex2' not in part3:
-                        if (part3 == 'ex.png' or 'ex#' in part3) and cg_ex[j] == 1:
+                    if part2 == name2 and 'ex' in part3:
+                        if (part3 == 'ex.png' or 'ex#' in part3 or 'ex1' in part3) \
+                                and cg_ex[j] == 1:
                             ex1 = Image.open(file_path + i)
                             print(part3, 'EX FOUND')
-                            base.alpha_composite(face1, (0, 0))
-                            base.alpha_composite(face2, (0, 0))
                             temp = base.copy()
                             temp.alpha_composite(ex1, (0, 0))
-                            temp.save(save_path + cg_list[j] + part3.replace('#0', ''))
-                            print(cg_list[j] + part3, 'MERGE COMPLETE\n')
-                        elif part3 == 'ex1#%03d.png' % index3 and cg_ex[j] == 2:
+                            if temp != Image.new('RGBA', img_size, (0, 0, 0, 0)):
+                                temp.save(save_path + cg_list[j] + part3.replace('#0', ''))
+                                print(cg_list[j] + part3, 'MERGE COMPLETE\n')
+                            else:
+                                print('merge error\n')
+                        elif (part3 == 'ex.png' or 'ex#' in part3 or 'ex1' in part3) \
+                                and cg_ex[j] == 2:
                             ex1 = Image.open(file_path + i)
                             print(part3, 'EX1 FOUND')
-                            base.alpha_composite(face1, (0, 0))
-                            temp = base.copy()
-                            temp.alpha_composite(ex1, (0, 0))
-                            temp.save(save_path + cg_list[j] + '.png')
-                            print(cg_list[j], 'MERGE COMPLETE\n')
-                        elif part3 == 'ex1#%03d.png' % index3 and cg_ex[j] == 3:
-                            ex1 = Image.open(file_path + i)
-                            print(part3, 'EX1 FOUND')
-                            base.alpha_composite(face1, (0, 0))
                             temp = base.copy()
                             temp.alpha_composite(ex1, (0, 0))
                             for k in all_file:
@@ -171,14 +159,17 @@ while n < len(txt_path):
                                     while m < len(all_parts_temp):
                                         part3_temp = part3_temp + all_parts_temp[m]
                                         m = m + 1
-                                if part2_temp == name2 and \
-                                        (part3_temp == 'ex2#%03d.png' % ex2_judge(index3) or part3_temp == 'ex2.png'):
+                                if part2_temp == name2 and 'ex2' in part3_temp:
                                     ex2 = Image.open(file_path + k)
                                     print(part3_temp, 'EX2 FOUND')
                                     temp2 = temp.copy()
                                     temp2.alpha_composite(ex2, (0, 0))
-                                    temp2.save(save_path + cg_list[j] + part3_temp.replace('#0', ''))
-                                    print(cg_list[j] + part3 + part3_temp, 'MERGE COMPLETE\n')
+                                    if temp2 != Image.new('RGBA', img_size, (0, 0, 0, 0)):
+                                        ext = part3.replace('#0', '').replace('.png', '') + part3_temp.replace('#0', '')
+                                        temp2.save(save_path + cg_list[j] + ext)
+                                        print(cg_list[j] + part3 + part3_temp, 'MERGE COMPLETE\n')
+                                    else:
+                                        print('merge error\n')
             status[j] = True
         # 找到图片素材并合成输出
         j = j + 1
--- README.md
+++ README.md
@@ -1,40 +1,44 @@
 # ToneWorks_CG_Merge
 
-Several python scripts for merging the CGs extracted from the games of tone work's
+Python scripts for merging the CGs extracted from the games of tone work's
 
-这个项目是用来合并tone work's社的游戏中的CG的，目前支持：初恋1/1、星织梦未来（仅Perfect Edition）、银色遥远（目前有bug，不建议使用）、在月之彼岸邂逅（含SSR）
+这个项目是用来合并tone work's社的游戏中的CG的，目前支持：初恋1/1、星织梦未来（仅Perfect Edition）、银色遥远、在月之彼岸邂逅（含SSR）
 
-This project is used to merge the CGs extracted from the games of tone work's， currently support：初恋1/1, 星織ユメミライ(Perfect Edition only), 銀色、遥か(caution: unfixed bugs)、月の彼方で逢いましょう(including SSR)
+This project is used to merge the CGs extracted from the games of tone work's， currently support：初恋1/1, 星織ユメミライ(Perfect Edition only), 銀色、遥か、月の彼方で逢いましょう(including SSR)
 
 注意，此脚本不包含解包功能，请先完成解包后再进行合成
 
 Caution: this script does not include the unpack function, please unpack the CGs first
 
+由于游戏中解包出的画廊使用的CG合成规则文件不含ex和ex2图层的数据，所以在合成银遥、月彼和月彼SSR时，如有这两个图层就直接进行了全排列，也因此会产生一些原本没有或明显图层错误的图片，自行删除即可
+
+Because the gallery CG merging rule files extracted from the games do not contain ex and ex2 layer data, so when merging 銀色、遥か、月の彼方で逢いましょう(including SSR), if there are ex and ex2 layers, it will be performed all permutations, which lead to some CGs don't exist in the games or have obvious layer errors, please delete them manually
+
 ## 使用方法
 
-#### 1. 解包合成规则/Unpack the merging rules
+### 1. 解包合成规则/Unpack the merging rules
 
 首先使用[SiglusExtract](https://github.com/xmoeproject/SiglusExtract)解包Scene.pck，然后在解包后的文件中找到如图所示命名方式的.txt文件，并移入本程序根目录下‘cg’文件夹中
 
 First, use [SiglusExtract](https://github.com/xmoeproject/SiglusExtract) to unpack Scene.pck, then find the .txt files named as shown in the picture, and move them to 'cg' folder of the root directory of this script
 
-<img src="./readme/01.png" width="100%">
+<img src="./readme/01.png" width="879" alt="">
 
-#### 2. 解包图片/Unpack CG parts
+### 2. 解包图片/Unpack CG parts
 
 由于Siglus引擎使用的图片封装格式.g00可以封入多张图片，因此我们需要把所有图片都解出来；这里推荐使用[GARbro](https://github.com/morkt/GARbro)，注意不要直接选“转换多媒体文件”，先看看能不能解压，否则封装了多张CG的g00只能转换成一张图片
 
 Cause the CGs are packaged in .g00 format, we need to extract all the CGs; here we recommend using [GARbro](https://github.com/morkt/GARbro), note that check if you can unpack the .g00 file before choosing “Convert multimedia files”, otherwise the g00 packaged with multiple CGs will only convert to one image
 
-不推荐继续使用Siglus的原因是，Siglus会把解出来的图片的透明部分裁掉，这样就无法定位遮罩图层的位置，不能直接合成
+不推荐继续使用SiglusExtract的原因是，SiglusExtract会把解出来的图片的透明部分裁掉，这样就无法定位遮罩图层的位置，不能直接合成
 
-The reason I do not recommend using Siglus is that Siglus will cut the transparent part of the extracted images, so that we cannot locate the mask layer, which means the script cannot work
+The reason I do not recommend using SiglusExtract is that SiglusExtract will cut the transparent part of the extracted images, so that we cannot locate the mask layer, which means the script cannot work
 
 解包后不要改变文件名，保持原样即可，脚本里合成采用的是GARbro解出来的图片名；将所有图片一并放入到‘cg’文件夹中，确保所有图片处在‘cg’文件夹的第一级目录下
 
 Do not change the file name after unpacking, cause the script will use the extracted image name from GARbro; Put all the images into 'cg' folder, and make sure that all the images are in the first level directory of 'cg' folder
 
-#### 3. 合成/Merge
+### 3. 合成/Merge
 
 确保完成上述步骤后，运行CGMerge.exe，选择你要合成CG的游戏，等待即可，完成的CG会输出到‘output’文件夹中；由于Python只能单线程运行，所以需要的时间可能会比较长，那就。。。等吧（
 
